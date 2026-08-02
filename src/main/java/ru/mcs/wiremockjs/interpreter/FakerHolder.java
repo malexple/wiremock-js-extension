5ddf6d6e268df21c836f845b6313a92f417a765c
malexple <malexple@gmail.com>
2026-08-02 17:54:57 +0300

    add ru locale fake data

--- src/main/java/ru/mcs/wiremockjs/interpreter/FakerHolder.java
+++ src/main/java/ru/mcs/wiremockjs/interpreter/FakerHolder.java
@@ -2,12 +2,14 @@ package ru.mcs.wiremockjs.interpreter;
 
 import net.datafaker.Faker;
 
+import java.util.Locale;
+
 public class FakerHolder {
     private static volatile Faker instance;
 
     public static synchronized Faker getInstance() {
         if (instance == null) {
-            instance = new Faker();
+            instance = new Faker(new Locale("ru"));
         }
         return instance;
     }
